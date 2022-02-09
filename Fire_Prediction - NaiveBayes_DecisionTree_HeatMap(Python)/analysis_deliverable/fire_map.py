a80b67bea25249e524401b46a351d77ecefe57dc
MaximoMoyer <41522480+MaximoMoyer@users.noreply.github.com>
2022-02-09 08:07:52 -0500

    Delete Fire_Prediction - NaiveBayes_DecisionTree_HeatMap(Python)/analysis_deliverable directory

--- Fire_Prediction - NaiveBayes_DecisionTree_HeatMap(Python)/analysis_deliverable/fire_map.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-
-fire_stats = pd.read_csv("C:/Users/Kolya/Documents/Fire/nfirs_data/fire_short.csv")
-#print(fire_stats)
-
-fg = fire_stats.groupby("ZIP5").count()
-print(fg.sort_values())
-#fg[["batch"]].to_csv("C:/Users/Kolya/Documents/Fire/nfirs_data/zip_counts.csv")
